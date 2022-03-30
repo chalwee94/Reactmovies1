a5bf3802a8c2272d6ec9e96177e91ae09c9504ec
patrick yoro <patrick.yoro@yahoo.no>
2022-03-30 03:16:04 +0200

    fixing som issues in my project

---
+++ _tests_/movies.test.jsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import {render} from "react-dom";
+import {ListMovies, NewMovie} from "../application";
+import {Simulate} from "react-dom/test-utils";
+import {MemoryRouter} from "react-router-dom";
+
+describe("Movies", () => {
+    it("runs shows movies", () => {
+        const movies = [
+            {
+                title: "when they see us ", year: 2019, plot: "judgment without prove"
+            },
+            {
+                title: "alien", year: 1979, plot: "Horror in space"
+            }
+        ]
+        const element = document.createElement("div");
+        render(<ListMovies movies={movies}/>, element);
+        expect(element.innerHTML).toMatchSnapshot();
+    });
+
+    it("creates new movie", () => {
+        const element = document.createElement("div");
+        const onAddMovie = jest.fn();
+        render(<MemoryRouter><NewMovie onAddMovie={onAddMovie}/></MemoryRouter>, element);
+        expect(element.innerHTML).toMatchSnapshot();
+        Simulate.change(element.querySelector("input"), {target: {value: "Some movie"}});
+        Simulate.submit(element.querySelector("form"));
+        expect(onAddMovie).toBeCalledWith({
+            title: "Some movie", year: "", plot: ""
+        })
+    })
+})
